11f1135aca17aa5107605927270f7e3cb79881d0
Wolfnj <nathan.wolf@rjyoung.com>
2022-06-16 14:33:33 -0400

    Apoints

--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Appointment } from '../src/Appointment';
+import { 
+  Appointment,
+  AppointmentsDayView
+ } from '../src/Appointment';
 
 describe('Appointment', () => {
   let container;
@@ -24,4 +27,48 @@ describe('Appointment', () => {
     render(<Appointment customer={customer} />);
     expect(container.textContent).toMatch('Jordan');
   });
+});
+
+describe('AppointmentsDayView',()=>{
+  let container;
+
+  beforeEach(() => {
+    container = document.createElement('div');
+  });
+
+  const render = (component) =>
+    ReactDOM.render(component, container);
+
+  it('renders a div with the right id',()=>{
+    render(<AppointmentsDayView appointments={[]} />)
+    expect(container.querySelector('div#appointmentsDayView')).not.toBeNull()
+  });
+
+  it('renders multiple appointments in an ol element',()=>{
+    const today = new Date();
+    const appointments =[
+      {startsAt: today.setHours(12,0) },
+      {startsAt: today.setHours(13,0) }
+    ];
+    render(<AppointmentsDayView appointments={appointments} />)
+    expect(container.querySelector('ol')).not.toBeNull();
+    expect(container.querySelector('ol').children).toHaveLength(2);
+  });
+
+  it('renders each appointment in an li',()=>{
+    const today = new Date();
+    const appointments = [
+      { startsAt: today.setHours(12, 0) },
+      { startsAt: today.setHours(13, 0) },
+    ];
+    render(<AppointmentsDayView appointments={appointments} />);
+    expect(container.querySelector("li").children).toHaveLength(2);
+
+    expect(container.querySelectorAll("li")[0].textContent).toEqual('12:00');
+    expect(container.querySelectorAll("li")[0].textContent).toEqual('13:00');
+
+
+  });
+
+
 });
